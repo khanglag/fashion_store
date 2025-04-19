c5644b50454e22d21e5489cd7654b2fa7572dc2f
Minh Khang <khangminh.do2003@gmail.com>
2025-04-19 22:32:59 +0700

    t

--- server/connection.php
+++ server/connection.php
@@ -1,11 +1,11 @@
 <?php
 
-define('DATABASE_SERVER', 'localhost');
-define('DATABASE_USER','root');
-define('DATABASE_PASSWORD','');
-define('DATABASE_NAME','php_project');
+// define('DATABASE_SERVER', 'localhost');
+// define('DATABASE_USER','root');
+// define('DATABASE_PASSWORD','');
+// define('DATABASE_NAME','php_project');
 
-$conn = mysqli_connect(DATABASE_SERVER, DATABASE_USER, DATABASE_PASSWORD, DATABASE_NAME);
+$conn = mysqli_connect("localhost", "root", '', "php_project");
 
 if (!$conn) {
     die("Kết nối thất bại: " . mysqli_connect_error());
