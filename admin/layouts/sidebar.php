4594da1ecf19c864539d4840852d365146febc32
Minh Khang <khangminh.do2003@gmail.com>
2025-04-21 18:50:20 +0700

    all

---
+++ admin/layouts/sidebar.php
@@ -0,0 +1,72 @@
+<aside class="main-sidebar sidebar-dark-primary elevation-4" style="position: fixed; top: 0; bottom: 0;">
+    <!-- Brand Logo -->
+    <a href="../admin/dashboard.php" class="brand-link">
+    <img src="../assets/images/logo.jpg" alt="AdminLTE Logo" class="brand-image img-circle elevation-2"
+    style="opacity: .8"> 
+
+  <span class="brand-text font-weight-light">Admin</span>
+
+    </a>
+    <!-- Sidebar -->
+    <div class="sidebar">
+        <!-- Sidebar user (optional) -->
+        <nav class="mt-4">
+            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                <!-- Add icons to the links using the .nav-icon class
+								with font-awesome or any other icon font library -->
+                <li class="nav-item">
+                    <a href="dashboard.php" class="nav-link">
+                        <i class="nav-icon fas fa-tachometer-alt"></i>
+                        <p>Dashboard</p>
+                    </a>
+                </li>
+              
+                <li class="nav-item">
+                    <a href="list_categorys.php" class="nav-link">
+                     <i class="nav-icon fas fa-star"></i>
+                        <p>Category</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="list_status_products.php" class="nav-link">
+                    <i class="nav-icon fas fa-fire"></i>
+                        <p>Status Products</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="list_products.php" class="nav-link">
+                        <i class="nav-icon fas fa-tag"></i>
+                        <p>Products</p>
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a href="list_orders.php" class="nav-link">
+                        <i class="nav-icon fas fa-shopping-bag"></i>
+                        <p>Orders</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="list_users.php" class="nav-link">
+                        <i class="nav-icon  fas fa-users"></i>
+                        <p>Users</p>
+                    </a>
+                </li>
+                  <li class="nav-item">
+                    <a href="list_slider.php" class="nav-link">
+                        <i class="nav-icon fas fa-image"></i>
+                        <p>Slider</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="../index.php" class="nav-link">
+                        <i class="nav-icon  fas fa-home"></i>
+                        <p>Back Home</p>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+        <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+</aside>
